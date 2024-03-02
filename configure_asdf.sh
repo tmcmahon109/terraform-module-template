681e5fc07766671b7e168c1d87ba0e8b4fbe264d
Trevor McMahon <trevormcmahon@Trevors-MacBook-Air.local>
2024-03-02 13:48:38 -0800

    Initial

---
+++ configure_asdf.sh
@@ -0,0 +1,14 @@
+#Install ASDF for package management.
+if [ -f $HOME/.asdf/asdf.sh ]; then
+    /bin/echo 'File exists...skipping'
+else
+    /usr/bin/git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+fi
+
+# Add to zshrc if using mac
+/bin/echo "Adding ASDF to .zshrc file."
+/bin/echo ". $HOME/.asdf/asdf.sh" > $HOME/.zshrc
+
+# Restart Shell
+/bin/echo "Restarting shell"
+exec zsh -l
